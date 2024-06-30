6efe2b9a988aa53fa2edcccc30fda9a33bc6c24f
Nafis Rayan <nafisrayan123@gmail.com>
2024-07-01 02:33:58 +0600

    audio not working... :")

--- hot_player/lib/pages/audio_page.dart
+++ hot_player/lib/pages/audio_page.dart
@@ -1,7 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-class AudioPage extends StatelessWidget {
+class AudioPage extends StatefulWidget {
   const AudioPage({Key? key}) : super(key: key);
+
+  @override
+  _AudioPageState createState() => _AudioPageState();
+}
+
+class _AudioPageState extends State<AudioPage> {
+  late AudioPlayer _audioPlayer;
+  bool _isPlaying = false;
+  Duration _duration = Duration.zero;
+  Duration _position = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    _audioPlayer.setSourceUrl('2.mp3');
+    _audioPlayer.onDurationChanged.listen((duration) {
+      setState(() {
+        _duration = duration;
+      });
+    });
+    _audioPlayer.onPositionChanged.listen((position) {
+      setState(() {
+        _position = position;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void _togglePlayback() {
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+    if (_isPlaying) {
+      _audioPlayer.resume();
+    } else {
+      _audioPlayer.pause();
+    }
+  }
+
+  void _seek(Duration position) {
+    _audioPlayer.seek(position);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +59,44 @@ class AudioPage extends StatelessWidget {
         title: Text('Audio'),
       ),
       body: Center(
-        child: Text('Audio Page Content'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _position.toString().split('.').first,
+              style: TextStyle(fontSize: 24),
+            ),
+            Slider(
+              value: _position.inSeconds.toDouble(),
+              min: 0,
+              max: _duration.inSeconds.toDouble(),
+              onChanged: (value) {
+                _seek(Duration(seconds: value.toInt()));
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.replay_5),
+                  onPressed: () {
+                    _seek(Duration(seconds: _position.inSeconds - 5));
+                  },
+                ),
+                IconButton(
+                  icon: _isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+                  onPressed: _togglePlayback,
+                ),
+                IconButton(
+                  icon: Icon(Icons.fast_forward),
+                  onPressed: () {
+                    _seek(Duration(seconds: _position.inSeconds + 5));
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
